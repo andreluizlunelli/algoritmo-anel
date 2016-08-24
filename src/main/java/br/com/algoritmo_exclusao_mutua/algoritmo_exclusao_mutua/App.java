3f1c00f8d8c973ac9f92e4162f72e36df2ff970b
unknown <allunelli@MC-4370.furb.br>
2016-08-23 21:42:23 -0300

    adicionado estrutura inicial

---
+++ src/main/java/br/com/algoritmo_exclusao_mutua/algoritmo_exclusao_mutua/App.java
@@ -0,0 +1,13 @@
+package br.com.algoritmo_exclusao_mutua.algoritmo_exclusao_mutua;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
